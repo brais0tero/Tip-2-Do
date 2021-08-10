a9473aacbae93014930c6d8d1044d70901bb7ecf
brais0tero <32261639+brais0tero@users.noreply.github.com>
2021-08-10 11:34:31 +0200

    Update administracion.php

---
+++ Web/paginasWIP/administracion.php
@@ -0,0 +1,4 @@
+<!-- Usar esta linea SQL para obtener informacion de tablas y auto generar el formulari
+
+
+SELECT table_name, COLUMN_NAME, DATA_TYPE FROM information_schema.COLUMNS WHERE table_schema = "tip2do" -->
